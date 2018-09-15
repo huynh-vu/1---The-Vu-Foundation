f1bc89b9f487b0c8ae198c88fa01600cba159408
huynh-vu <huynh-vu@users.noreply.github.com>
2018-09-15 01:05:49 -0500

    updated website

--- update_website.cmd
+++ update_website.cmd
@@ -1,6 +1,4 @@
-cd "C:\Users\stock9\The Vu Foundation Dropbox\3 - Gulf Bay Marine\website-gh-pages\website-gh-pages"
-rscript raise_starbucks.r
-rscript stocks.r
+cd C:\Users\stock9\The Vu Foundation Dropbox\1 - The Vu Foundation
 git add .
 git commit -m "updated website"
 git push
